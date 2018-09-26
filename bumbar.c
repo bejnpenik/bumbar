8a86e6cd1038757ba2b6f0cb857fd42fcd4a2538
Branislav Panić <panicbranislav@gmail.com>
2018-09-26 18:21:07 +0200

    Update bumbar.c

--- bumbar.c
+++ bumbar.c
@@ -12,15 +12,19 @@
 #include "task.h"
 #include "desktop.h"
 #include "event.h"
-#define FIFO_NAME "/tmp/bumbar_fifo"
-xcb_connection_t *dpy;
-xcb_screen_t *screen;
-int default_screen;
-xcb_window_t root;
-xcb_ewmh_connection_t *ewmh;
-int run;
 
-int DAEMON = 1;
+#ifndef FIFO_NAME
+#define FIFO_NAME "/tmp/bumbar_fifo"
+#endif
+
+static xcb_connection_t *dpy;
+static xcb_screen_t *screen;
+static int default_screen;
+static xcb_window_t root;
+static xcb_ewmh_connection_t *ewmh;
+static int run;
+
+static int DAEMON = 0;
 void handle_signal(int sig)
 {
     if (sig == SIGTERM || sig == SIGINT || sig == SIGHUP)
@@ -62,7 +66,6 @@ void print_output(desktop_t *desktop_list, int nbr_of_desktops){
 				printf("\tU: win:%d, title: %s\n", desktop_list[i].tasks[j].win, desktop_list[i].tasks[j].title);
 			
 		}
-		//printf("\n");
 	}
 }
 
@@ -93,13 +96,112 @@ void print_output_to_fifo(desktop_t *desktop_list, int nbr_of_desktops){
 				strcat(write_arr, tmp_arr);
 				
 			}
-			//printf("\n");
 		}
 		write(fifo_fd, write_arr, strlen(write_arr)+1);
 		close(fifo_fd);
 	}
 }
+
+int cleanup(task_t *task_list, int nbr_of_tasks, desktop_t *desktop_list, int nbr_of_desktops){
+	for (int i = 0; i < nbr_of_desktops; i++) {
+		free(desktop_list[i].name);
+		free(desktop_list[i].tasks);
+	}
+	free(desktop_list);
+	for (int i = 0; i < nbr_of_tasks; i++) free(task_list[i].title);
+	free(task_list);
+	xcb_ewmh_connection_wipe(ewmh);
+    	free(ewmh);
+	xcb_disconnect(dpy);	
+}
+int make_fifo(void){
+	if (access(FIFO_NAME, F_OK) == -1) {
+		int res = mkfifo(FIFO_NAME, 0777);
+		if (res != 0) {
+		    return 1;
+		}
+    	}
+	return 0;
+}
+void continuos_output( int deamonize, task_t *task_list, int nbr_of_tasks, desktop_t *desktop_list, int nbr_of_desktops){
+	fd_set descriptors;xcb_generic_event_t *evt;int print=0;
+	register_root_events(dpy, screen, &run);
+	int dpy_fd = xcb_get_file_descriptor(dpy);
+	int sel_fd = MAX(dpy_fd, -1) + 1;
+	while (run){
+		FD_ZERO(&descriptors);
+		FD_SET(dpy_fd, &descriptors);
+		if (select(sel_fd, &descriptors, NULL, NULL, NULL)) {
+			if (FD_ISSET(dpy_fd, &descriptors))
+				while ((evt = xcb_poll_for_event(dpy)) != NULL) {
+					print = event_handler(ewmh, default_screen, event_generator(ewmh, screen, evt), task_list, &nbr_of_tasks, desktop_list, &nbr_of_desktops);
+					if (print==1){
+						if (deamonize==1){						
+							print_output_to_fifo(desktop_list, nbr_of_desktops);
+						}
+						else{
+							print_output(desktop_list, nbr_of_desktops);
+						}
+				
+					}
+					free(evt);
+				}
+			}
+	}	
+	cleanup(task_list,  nbr_of_tasks, desktop_list, nbr_of_desktops);
+		
+}
+void single_output(task_t *task_list, int nbr_of_tasks, desktop_t *desktop_list, int nbr_of_desktops){
+	print_output(desktop_list, nbr_of_desktops);
+	cleanup(task_list, nbr_of_tasks, desktop_list, nbr_of_desktops);
+}
 int main(int argc, char **argv){
+	int deamonize = 0, continuos = 0;
+	int desktops=0, tasks=0;
+	if (argc == 2){
+		if (strcmp(argv[1], "-c")==0) continuos = 1;
+		if (strcmp(argv[1], "-D")==0) deamonize = 1;
+	}
+	setup();
+	signal(SIGTERM, handle_signal);
+	signal(SIGINT, handle_signal);
+	signal(SIGHUP, handle_signal);
+	task_t *task_list = create_task_list();
+	desktop_t *desktop_list = create_desktop_list();
+	task_event_t event;
+	event.event = TASK_LIST_CHANGE;
+	event.win = root;
+	desktop_event_t devent;
+	devent.event = DESKTOP_NUMBER_CHANGE;
+	devent.win = root;
+	tasks = update_task_list(ewmh, default_screen, event, task_list, tasks);
+	desktops = update_desktop_list(ewmh, default_screen, devent, task_list, tasks, desktop_list, desktops);
+	if (task_list == NULL){
+		return 1;
+	}
+	if (deamonize == 1){
+		if (make_fifo()) {
+			printf("HERE");
+			cleanup(task_list, tasks, desktop_list, desktops);
+			return 1;
+		}
+		run = 1;
+		daemon(1, 1);
+		continuos_output(deamonize, task_list, tasks, desktop_list, desktops);
+	}
+	if (continuos == 1){
+		run = 1;
+		continuos_output(deamonize, task_list, tasks, desktop_list, desktops);
+	}
+	if (deamonize != 1 && continuos != 1) single_output( task_list, tasks, desktop_list, desktops);
+	return 0;
+}
+int main_o(int argc, char **argv){
+	int deamonize = 0, continuos = 0;
+	if (argc == 2){
+		if (strcmp(argv[1], "-c")) continuos = 1;
+		if (strcmp(argv[1], "-D")) deamonize = 1;
+	}
 	setup();
 	signal(SIGTERM, handle_signal);
 	signal(SIGINT, handle_signal);
@@ -115,8 +217,6 @@ int main(int argc, char **argv){
 	event.event = TASK_LIST_CHANGE;
 	event.win = root;
 	nbr_of_tasks = update_task_list(ewmh, default_screen, event, task_list, nbr_of_tasks);
-	for (int i = 0; i < nbr_of_tasks; i++) printf("%s\n", task_list[i].title);
-	
 	desktop_t *desktop_list = create_desktop_list();
 	if (desktop_list == NULL){
 		printf("HERE!");
@@ -126,15 +226,6 @@ int main(int argc, char **argv){
 	devent.event = DESKTOP_NUMBER_CHANGE;
 	devent.win = root;
 	int nbr_of_desktops = update_desktop_list(ewmh, default_screen, devent, task_list, nbr_of_tasks, desktop_list, 0);
-	for (int i = 0; i < nbr_of_desktops; i++) printf("%s\n", desktop_list[i].name);
-	print_output(desktop_list, nbr_of_desktops);
-	/*for (int i = 0; i < nbr_of_desktops; i++) {
-		free(desktop_list[i].name);
-		free(desktop_list[i].tasks);
-	}
-	free(desktop_list);
-	for (int i = 0; i < nbr_of_tasks; i++) free(task_list[i].title);
-	free(task_list);*/
 	xcb_generic_event_t *evt;
 	run = 1;
 	int print = 0;
@@ -144,12 +235,12 @@ int main(int argc, char **argv){
 	fd_set descriptors;
 	if (DAEMON){
 		if (access(FIFO_NAME, F_OK) == -1) {
-        res = mkfifo(FIFO_NAME, 0777);
-        if (res != 0) {
-            fprintf(stderr, "Could not create fifo %s\n", FIFO_NAME);
-            exit(EXIT_FAILURE);
-        }
-    }
+			res = mkfifo(FIFO_NAME, 0777);
+			if (res != 0) {
+			    fprintf(stderr, "Could not create fifo %s\n", FIFO_NAME);
+			    exit(EXIT_FAILURE);
+			}
+    		}
 		daemon(1, 0);
 	}
 	while (run){
@@ -177,9 +268,10 @@ int main(int argc, char **argv){
 		free(desktop_list[i].tasks);
 	}
 	free(desktop_list);
+	for (int i = 0; i < nbr_of_tasks; i++) free(task_list[i].title);
 	free(task_list);
 	xcb_ewmh_connection_wipe(ewmh);
-    free(ewmh);
+    	free(ewmh);
 	xcb_disconnect(dpy);
 	return 0;
 }
